9596980d968cc2ee8cbf5762bbcf71c63821d5a5
Herald Server <webdirector@badgerherald.com>
2013-09-17 20:28:07 -0500

    Add analytics to footer

--- footer.php
+++ footer.php
@@ -30,5 +30,18 @@
 	<?php /* TODO:  Do this in a WP way */ ?>
 	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/exa.js" type="text/javascript"></script>
+	<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-2337436-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
 </body>
 </html>
