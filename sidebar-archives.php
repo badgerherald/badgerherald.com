5d02885192ef545c8625537bf5a65c5c32ebc387
Zach Thomae <zthomae@gmail.com>
2013-10-07 17:26:08 -0500

    Top stories with ads

--- sidebar-archives.php
+++ sidebar-archives.php
@@ -11,12 +11,34 @@
 <div id="sidebar">
 <div class="sidebar-inner">
 
+<?php hrld_sidebar_ad(); ?>
+
 <h1 class="most-recent-headline">Most read this week</h1>
 
 <div class="sidebar-scroll">
 
-<?php wpp_get_mostpopular("range=weekly&post_type=sports,oped,artsetc,news&stats_comments=0&limit=10&wpp_start='<ul class=\"sidebar-posts\">'&post_html='<li class=\"post-in-sidebar\"><a class=\"post-permalink trending-story\" href=\"{url}\">{text_title}</li>'"); ?>
+<?php 
+
+    $wpp_query = array(
+        'range=weekly'
+    ,   'post_type=sports,oped,artsetc,news'
+    ,   'stats_comments=0'
+    ,   'limit=10'
+    ,   "wpp_start='<ul class=\"sidebar-posts\">'"
+    ,   "post_html='
+            <li class=\"post-in-sidebar\">
+                <a class=\"post-permalink trending-story\" href=\"{url}\">{text_title}</a>
+            </li>'"
+    );
+    $wpp_query_string = join('&', $wpp_query);
+
+    wpp_get_mostpopular($wpp_query_string);
+
+?>
+
+<?php hrld_sidebar_lower_ad(); ?>
 
 </div><!-- class="sidebar-scroll" -->
+
 </div><!-- class="inner-sidebar" -->
 </div><!-- id="sidebar" -->
