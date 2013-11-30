448bad063739626d02a2632b3075c091e345f093
Will Haynes <haynes24@gmail.com>
2013-11-30 16:34:51 -0600

    Queries for news created

--- index.php
+++ index.php
@@ -24,15 +24,65 @@ get_header();
 
 		</div>
 
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet. Duis sagittis ipsum. Praesent mauris. Fusce nec tellus sed augue semper porta. Mauris massa. Vestibulum lacinia arcu eget nulla. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Curabitur sodales ligula in libero. </p>
+		<?php
+		
+			/* Build query for featured stories in news */
 
-		<p>Sed dignissim lacinia nunc. Curabitur tortor. Pellentesque nibh. Aenean quam. In scelerisque sem at dolor. Maecenas mattis. Sed convallis tristique sem. Proin ut ligula vel nunc egestas porttitor. Morbi lectus risus, iaculis vel, suscipit quis, luctus non, massa. Fusce ac turpis quis ligula lacinia aliquet. Mauris ipsum. Nulla metus metus, ullamcorper vel, tincidunt sed, euismod in, nibh. Quisque volutpat condimentum velit. </p>
+			$args = array();
+			$args['tax_query'] = array(
+	                array(
+	                    'taxonomy' => 'importance',
+	                    'field' => 'slug',
+	                    'terms' => array('featured'),
+	                    'operator' => 'IN'
+	                )
+	            );
+			$args['post_type'] = 'news';
+			$args['posts_per_page'] = 4;
 
-		<p>Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nam nec ante. Sed lacinia, urna non tincidunt mattis, tortor neque adipiscing diam, a cursus ipsum ante quis turpis. Nulla facilisi. Ut fringilla. Suspendisse potenti. Nunc feugiat mi a tellus consequat imperdiet. Vestibulum sapien. Proin quam. Etiam ultrices. Suspendisse in justo eu magna luctus suscipit. </p>
+			$news_featured = new WP_Query( $args );
+			$excludenews = array();
+		?>
+		<h3>Featured Stories</h3>
+		<?php while( $news_featured->have_posts() ) {
+			$news_featured->next_post();
+			echo '<li>' . get_the_title( $news_featured->post->ID ) . '</li>';
+			$excludenews[] = $news_featured->post->ID;
+		} ?>
 
-		<p>Sed lectus. Integer euismod lacus luctus magna. Quisque cursus, metus vitae pharetra auctor, sem massa mattis sem, at interdum magna augue eget diam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Morbi lacinia molestie dui. Praesent blandit dolor. Sed non quam. In vel mi sit amet augue congue elementum. Morbi in ipsum sit amet pede facilisis laoreet. Donec lacus nunc, viverra nec, blandit vel, egestas et, augue. Vestibulum tincidunt malesuada tellus. Ut ultrices ultrices enim. </p>
+		<?php
+			// Restore original Post Data
+			wp_reset_postdata();
+		?>
 
-		<p>Curabitur sit amet mauris. Morbi in dui quis est pulvinar ullamcorper. Nulla facilisi. Integer lacinia sollicitudin massa. Cras metus. Sed aliquet risus a tortor. Integer id quam. Morbi mi. Quisque nisl felis, venenatis tristique, dignissim in, ultrices sit amet, augue. Proin sodales libero eget ante. Nulla quam. Aenean laoreet. Vestibulum nisi lectus, commodo ac, facilisis ac, ultricies eu, pede. </p>
+
+		<?php
+		
+			/* Build query for featured stories in news */
+			$args['post_type'] = 'news';
+			$args['posts_per_page'] = 40;
+			$args['post__not_in'] = $excludenews;
+
+			$news_featured = new WP_Query( $args );
+		
+		?>
+		
+		<h3>Recent</h3>
+		<?php while( $news_featured->have_posts() ) {
+			$news_featured->next_post();
+			echo '<li>' . get_the_title( $news_featured->post->ID ) . '</li>';
+		} ?>
+
+		<?php
+			// Restore original Post Data
+			wp_reset_postdata();
+		?>
+
+		
+
+
+
+
 
 
 
