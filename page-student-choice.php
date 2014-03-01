213296cdb88478080c7b55876d779256359c35d6
Matt Neil <mneil@wi.rr.com>
2014-02-28 22:27:41 -0600

    starter form styling

---
+++ page-student-choice.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages and that other
+ * 'pages' on your WordPress site will use a different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+get_header(); ?>
+
+		<div id="content" class="site-content" role="main">
+
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<header class="entry-header">
+						<?php if ( has_post_thumbnail() && ! post_password_required() ) : ?>
+						<div class="entry-thumbnail">
+							<?php the_post_thumbnail(); ?>
+						</div>
+						<?php endif; ?>
+
+						<h1 class="entry-title"><?php the_title(); ?></h1>
+					</header><!-- .entry-header -->
+
+					<div class="entry-content">
+						<form action="" method="post" class="quiz-container">
+						<?php
+							for($i = 0; $i < 6; $i++){
+								echo '<div class="quiz-question clearfix">';
+								echo '<div class="question-title"><h3>Question '.$i.'</h3><br /></div>';
+								echo '<ul class="answer-list">';
+								for($j = 0; $j < 6; $j++){
+									echo '<li><div class="answer-box"><input name="hrld_student_choice_'.$i.'" id="hrld_student_choice_'.$i.'_'.$j.'" type="radio" value="question: '.$i.'; answer: '.$j.'"><label for="hrld_student_choice_'.$i.'_'.$j.'"><img src="http://placekitten.com/180/180" /><span class="answer-description">The Badger Herald</span></label></div></li>';
+								}
+								echo '</ul>';
+								echo '</div>';
+							}
+						?>
+						</form>
+					</div><!-- .entry-content -->
+				</article><!-- #post -->
+
+		</div><!-- #content -->
+<?php get_footer(); ?>
