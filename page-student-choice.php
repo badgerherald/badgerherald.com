a2892cf733e548b17285a35c9faebe0147b03a3f
Matt Neil <mneil@wi.rr.com>
2014-03-03 19:23:47 -0600

    added images for question titles, must become buzzfeed

--- page-student-choice.php
+++ page-student-choice.php
@@ -22,14 +22,14 @@ get_header(); ?>
 
 					<div class="entry-content">
 					<?php
-						$display_form = false;
+						$display_form = true;
 						if($display_form):
 					?>
 						<form action="" method="post" class="quiz-container">
 						<?php
 							for($i = 0; $i < 6; $i++){
 								echo '<div class="quiz-question clearfix">';
-								echo '<div class="question-title"><h3>Question '.$i.'</h3><br /></div>';
+								echo '<div class="question-title"><img src="http://www.placecage.com/c/640/180"></div>';
 								echo '<ul class="answer-list">';
 								for($j = 0; $j < 6; $j++){
 									echo '<li class="inactive answer-box quiz_shadow"><input name="hrld_student_choice_'.$i.'" id="hrld_student_choice_'.$i.'_'.$j.'" type="radio" value="question: '.$i.'; answer: '.$j.'"><label for="hrld_student_choice_'.$i.'_'.$j.'"><img src="http://placecage.com/180/180" /><span class="description-wrap"><span class="checkmark-wrap quiz_shadow"><span class="answer-checkmark">&#10003;</span></span><span class="answer-description">The Badger Herald</span></span></label></li>';
