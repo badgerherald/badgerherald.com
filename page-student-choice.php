019edc9ce8e906ed9c3500e62e65818f91645d4e
Matt Neil <mneil@wi.rr.com>
2014-03-05 23:58:31 -0600

    added styling and error

--- page-student-choice.php
+++ page-student-choice.php
@@ -220,12 +220,12 @@ function valid_wisc($email) {
 								<p>Follow us on Twitter and Facebook to be updated on the winners.</p>
 								<div class="social-buttons">
 									<div class="twitter">
-										<a href="https://twitter.com/badgerherald" class="twitter-follow-button" data-show-count="false">Follow @badgerherald</a>
+										<a href="https://twitter.com/badgerherald" class="twitter-follow-button" data-show-count="false" data-size="large">Follow @badgerherald</a>
 										<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 									</div><!-- .twitter -->
 
 									<div class="facebook">
-										<div class="fb-like" data-href="http://facebook.com/badgerherald" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false"></div>
+										<div class="fb-like" data-href="http://facebook.com/badgerherald" data-layout="button_count" data-action="like" data-show-faces="false" data-share="false" data-height="28" data-width="120"></div>
 									</div><!-- .facebook -->
 								</div>
 							</div>
