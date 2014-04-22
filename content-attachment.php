a3dbf3045c45e30364197432031d67ab85371474
Matt Neil <mneil@wi.rr.com>
2014-04-22 18:21:14 -0500

    fixed image thumbnail size on author pages

--- content-attachment.php
+++ content-attachment.php
@@ -12,7 +12,7 @@
 <article id="post-<?php the_ID(); ?>" <?php post_class("stream-post instream-stream-post attahment-fullstream-post"); ?>>
 	<a class="summary-post-link" href="<?php the_permalink(); ?>" rel="bookmark">
 		<div class="entry-thumbnail">
-			<?php echo wp_get_attachment_image($post->ID, array(get_option( 'thumbnail_size_h' ),get_option( 'thumbnail_size_w' ))); ?>
+			<?php echo wp_get_attachment_image($post->ID, 'large-thumbnail'); ?>
 		</div>
 
         
