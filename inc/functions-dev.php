545437123a23056372fe64c66867eafab86ca671
Will Haynes <haynes24@gmail.com>
2014-11-19 22:15:32 -0600

    Pull remote images if HRLD_DEV is set.

---
+++ inc/functions-dev.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Functions to aid local development,
+ * particularly for Badger Herald developers.
+ *
+ * @package exa
+ */
+
+
+/**
+ * If hrld_dev is set, replaces whatever (local)host an attachment_url
+ * is using with badgerherald.com
+ */
+function exa_dev_attachment_url( $link ) {
+
+	$url = parse_url( $link );
+	if( HRLD_DEV && !strpos( $url['host'], 'badgerherald.com') ) {
+		$link = preg_replace('/.*uploads/', 'http://badgerherald.com/media', $link); 
+	}
+	return $link;
+
+}
+add_filter('wp_get_attachment_url', 'exa_dev_attachment_url');
