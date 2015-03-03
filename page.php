cbcc7ff832c6b0f7c85d66f86350dd65d19ce824
Will Haynes <haynes24@gmail.com>
2015-03-02 18:59:17 -0600

    add page.php back

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * The Template for displaying all single posts.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Thirteen
+ * @since Twenty Thirteen 1.0
+ */
+
+// Set up ads for current page.
+global $DoubleClick;
+
+get_header('minimal'); 
+get_template_part( 'inc/block', 'leaderboard' );
+?>
+
+<?php /* The loop */ ?>
+
+<?php while ( have_posts() ) : the_post(); ?>
+
+	<?php get_template_part( 'content', get_post_format() ); ?>
+
+    <div class="clearfix"></div>
+
+    <?php get_template_part('inc/block','billboard'); ?>
+    <?php get_template_part('inc/block','cover'); ?>
+
+    <div class="clearfix"></div>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
