9c6248301a088d9b721736b9f1b2a9c99fb694a3
Jason Jan <jasonjan2003@gmail.com>
2015-12-02 17:45:35 -0600

    search results look a lot nicer now. and WORKS.

--- inc/blocks/search-results.php
+++ inc/blocks/search-results.php
@@ -5,7 +5,7 @@
  *
  */
 ?>
-<div class="block alert-banner-block">
+<div class="block search-block">
     
     <div class="wrapper">
     <script>
@@ -17,29 +17,13 @@
 		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
 		})();
 	</script>
-	
-	<!-- Place this tag where you want both of the search box and the search results to render -->
-	<gcse:search></gcse:search>
 
 		<div class="gcs_container">
 
-			<gcse:searchresults-only
+			<gcse:search 
 				gname="largoGCSE"
 				overlayResults="false"
 				queryParameterName="s"></gcse:searchresults-only>
-
-			<script>
-
-				(function() {
-					var cx = '017748836545253253952:wjly4mzvsyu';
-
-					var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-					gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
-					    '//cse.google.com/cse.js?cx=' + cx;
-					var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
-					})();
-
-			</script>
 
 		</div>
 
