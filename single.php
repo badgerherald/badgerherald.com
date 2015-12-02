8b67de287ac75ce0cd18955e0133d932c0c08722
Jason Chan <jasonjan2003@users.noreply.github.com>
2015-12-02 10:45:58 -0600

    Revert "enhancement for #93"

--- single.php
+++ single.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(exa_is_video_post()) {
-	get_template_part('inc/templates/article-video');
+	get_template_part('inc/templates/video');
 } else {
 	get_template_part('inc/templates/article');
 }
