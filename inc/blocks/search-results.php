d6b74935b617f50c1655c5d089490c1898ca9b22
Will Haynes <haynes24@gmail.com>
2015-12-02 19:16:31 -0600

    Real issue with search.
    Protocol mismatch. Get rid of the protocol.

--- inc/blocks/search-results.php
+++ inc/blocks/search-results.php
@@ -12,8 +12,7 @@
 		(function() {
 		  var cx = '017748836545253253952:wjly4mzvsyu'; // Insert your own Custom Search engine ID here
 		  var gcse = document.createElement('script'); gcse.type = 'text/javascript'; gcse.async = true;
-		  gcse.src = (document.location.protocol == 'https' ? 'https:' : 'http:') +
-		      '//cse.google.com/cse.js?cx=' + cx;
+		  gcse.src = '//cse.google.com/cse.js?cx=' + cx;
 		  var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(gcse, s);
 		})();
 	</script>
