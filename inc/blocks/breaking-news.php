9ea878bde0a6ac5fb186b060ecece0b245f1a4a3
Aaron Wilson <awilson9@wisc.edu>
2015-12-08 23:22:39 -0600

    Life of post
    breaking post will stop displaying if it is  > 24 hours old

--- inc/blocks/breaking-news.php
+++ inc/blocks/breaking-news.php
@@ -17,24 +17,31 @@
     );
     $my_query = new WP_Query( $query_args );
 
-    if ($my_query->have_posts() ) :
+    if ($my_query->have_posts()) :
         while ( $my_query->have_posts() ) : $my_query->the_post();  ?>
-
-        <div class="block breaking-news-block">
-            <div class="wrapper">
-                <span class="BreakingBox">
-                   breaking news:
-                   </span>    
-                <span class="postbox">
-                    <a href="<?php the_permalink(); ?>" class="link">
-                     <?php the_title(); ?>
-                    </a>
-                    <span class="updated">UPDATED: <?php the_time(); ?></span>
-                        
-                </span>
-                <div style="clear: both;"></div>
-            </div>
-        </div>
+            
+            <?php $currentTime = time(); 
+            $local_timestamp = get_the_time('U');
+            $seconds = 86400; 
+            if (($currentTime - $local_timestamp)  < $seconds) : ?>
+           
+                <div class="block breaking-news-block" >
+                    <div class="wrapper">
+                        <span class="BreakingBox">
+                         breaking news:
+                           </span>    
+                        <span class="postbox">
+                            <a href="<?php the_permalink(); ?>" class="link">
+                             <?php the_title(); ?>
+                            </a>
+        
+                            <span class="updated">UPDATED: <?php the_time(); ?></span>
+                                
+                        </span>
+                        <div style="clear: both;"></div>
+                    </div>
+                </div>
+           <?php endif; ?>
     <?php
         endwhile;
     endif;
