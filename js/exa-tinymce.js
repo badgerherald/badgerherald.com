bb75f5463ac4d5f93aaa62598f3cc5083759a7ab
Jason Jan <jasonjan2003@gmail.com>
2015-12-10 21:16:21 -0600

    nicer admin ui for adding pull quotes, nicer styling.

--- js/exa-tinymce.js
+++ js/exa-tinymce.js
@@ -10,21 +10,65 @@
 
             ed.addCommand('pullquote', function() {
                 var selected_text = ed.selection.getContent(),
-                    byline = selected_text.split( new RegExp(/--/));
-                var return_text = '';
+                    selected_texts = selected_text.split( new RegExp(/--/)),
+                    return_text = '';
 
-                    if( byline.length > 1){
-                        quote = byline[0];
-                        byline = '<span class="quoteby"> &mdash;&nbsp;' + byline[ (byline.length -1)] + '</span>';
-                    }
-                    else{
+                var quote = '', quoteby = '', align = '';
+                if( selected_text != ''){
+
+                    if( selected_texts.length > 1){
+                        quote = selected_texts[0];
+                        quoteby = selected_texts[ (selected_texts.length - 1)];
+                    }else{
                         quote = selected_text;
-                        byline = '';
                     }
+                }
+                ed.windowManager.open({
+                                        title: 'Insert Pullquote',
+                                        width: 500,
+                                        height: 400,
+                                        body: [
+                                            {
+                                                type: 'textbox',
+                                                name: 'quote',
+                                                label: 'Quote',
+                                                multiline: true,
+                                                style: 'height: 100px;',
+                                                'value': quote.replace(/"/g, '')
+                                            },
+                                            {
+                                                type: 'textbox',
+                                                name: 'quoteby',
+                                                label: 'Quote By',
+                                                'value': quoteby
+                                            },
+                                            {
+                                                type: 'listbox',
+                                                name: 'align',
+                                                label: 'Align',
+                                                'values': [
+                                                    {text: 'Left', value: 'alignleft'},
+                                                    {text: 'Center', value: 'aligncenter'},
+                                                    {text: 'Right', value: 'alignright'}
+                                                ]
+                                            }
 
-                
-                return_text = '<div class="pullquote"><span class="quote">' + quote + '</span>' + byline +'</div>';
-                ed.execCommand('mceInsertContent', 0, return_text);
+
+                                        ],
+                                        onsubmit: function( event) {
+                                            quote = event.data.quote.replace(/"/g, '');
+                                            quoteby = event.data.quoteby;
+                                            align = event.data.align || '';
+                                            return_text  = '<div class="pullquote '+ align +'">';
+                                            return_text +=      '<span class="quote">' + quote + '</span>';
+                                            return_text +=      '<span class="quoteby"> &mdash;&nbsp;' + quoteby + '</span>';
+                                            return_text += '</div>';
+                                            ed.insertContent(return_text);
+
+                                             //editor.insertContent('Title: ' + e.data.title);
+                                        }
+                });
+
             });
 
         },
