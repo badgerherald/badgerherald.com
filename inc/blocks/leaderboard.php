f5dbd85fba365047b7ece64bb8ee385f29307802
Will Haynes <haynes24@gmail.com>
2016-03-05 21:58:40 -0600

    Leaderboard, behave

--- inc/blocks/leaderboard.php
+++ inc/blocks/leaderboard.php
@@ -1,18 +1,18 @@
 <?php global $OnCampus ?>
 
 <div class="block leaderboard-block">
-	
 	<div class="wrapper">
+		<div class="ad">
 	
-		<?php 
+			<?php 
 
-		$OnCampus->place_ad(array(
+			$OnCampus->place_ad(array(
 								'mobile' => 'mobile-leaderboard',
 								'tablet' => 'leaderboard'
-							)); 
+								)); 
 
-		?> 
-
+			?>
+			 
+		</div>
 	</div>
-
 </div>
