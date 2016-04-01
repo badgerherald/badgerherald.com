05cba2fa91ccc4c3ae12ddf085a92d3e81d05c3a
Aaron Wilson <awilson9@wisc.edu>
2016-03-31 22:00:42 -0500

    dirty bird
    the bird man

---
+++ inc/blocks/dirty-bird.php
@@ -0,0 +1,25 @@
+
+<div class="block dirty-bird-block" style='
+height:400px;
+position:absolute;'>
+<div class="wrapper"
+style='position:relative;'>
+
+
+
+<a href="dirtybird.badgerherald.com" >
+
+<img src = "" >
+<img src = "http://www.animatedimages.org/data/media/591/animated-parrot-image-0130.gif" 
+	style='
+
+	position:absolute;
+	bottom:-36px;
+	float:right;
+'
+>
+
+
+</a>
+</div>
+</div>
