896b647378645d7472d07f5225c2e9b5cc05d761
Aaron Wilson <awilson9@wisc.edu>
2016-04-01 00:54:10 -0500

    dirty-bird-final
    should be good to go

--- inc/blocks/dirty-bird.php
+++ inc/blocks/dirty-bird.php
@@ -1,23 +1,139 @@
+<style>
+@media(min-width: 0px){
+	a:hover{
+		opacity:.1;
 
-<div class="block dirty-bird-block" style='
-'>
-<div class="wrapper"
-style='position:relative;'>
+	}
+	.mobile{
+		display:block;
+		overflow:hidden;
+		
+		width:200px;
+		height:auto;
 
+	}
+	.wrapper{
+		position:relative;
+	}
+	.tablet{
+		display:none;
+	}
+	.desktop{
+		display:none;
+	}
+	.xl{
+		display:none;
+	}
+	.bird{
+		
+		position:absolute;
+		bottom:-250px;
+	}
 
+}
+@media (min-width: 760px) {
+	a:hover{
+		opacity:.1;
+	}
+	.mobile{
+		display:none;
+	}
+	.wrapper{
+		position:relative;
+	}
+	.tablet{
+		display:block;
+	}
+	.desktop{
+		display:none;
+	}
+	.xl{
+		display:none;
+	}
+	.bird{
 
-<a href="dirtybird.badgerherald.com" >
-
-<img src="<?php bloginfo('template_url') ?>/img/dirty-bird/xl.png" />
-<img src = "http://www.animatedimages.org/data/media/591/animated-parrot-image-0130.gif" 
-	style='
-	transform:scale(0.5, 0.5);
+	width:300px;
+	height:auto;
 	position:absolute;
-	bottom:0;
-	right:0;
+	bottom:-70px;
+	right:-10px;
 	align:right;
-'
-/>
+	
+}
+
+}
+@media (min-width: 1060px){
+	.mobile{
+		display:none;
+	}
+
+	.tablet{
+		display:none;
+	}
+	.desktop{
+		display:block;
+	}
+	.xl{
+		display:none;
+	}
+
+
+.wrapper{
+	position:relative;
+}
+.dirtybird{
+	display:block;
+	
+	
+}
+.bird{
+	
+	width:300px;
+	height:auto;
+	position:absolute;
+	bottom:-60px;
+	right:-20px;
+	align:right;
+
+}
+}
+@media (min-width: 1220px){
+	.mobile{
+		display:none;
+	}
+	.tablet{
+		display:none;
+	}
+	.desktop{
+		display:none;
+	}
+	.xl{
+		display:block;
+	}
+
+
+}
+
+
+
+</style>
+
+<div class="block dirty-bird-block">
+<div class="wrapper">
+
+
+
+
+<a href="http://dirtybird.badgerherald.com">
+<img src = "<?php bloginfo('template_url') ?>/img/dirty-bird/mobile.jpg" class="mobile">
+<img src = "<?php bloginfo('template_url') ?>/img/dirty-bird/tablet.jpg" class="tablet">
+<img src = "<?php bloginfo('template_url') ?>/img/dirty-bird/desktop.jpg" class="desktop">
+<img src = "<?php bloginfo('template_url') ?>/img/dirty-bird/xl.png" class="xl">
+
+
+
+<img src = "http://www.animatedimages.org/data/media/591/animated-parrot-image-0130.gif" 
+	class='bird'>
 
 
 </a>
--- index.php
+++ index.php
@@ -7,21 +7,21 @@
 
 get_header();
 
-get_template_part('inc/blocks/leaderboard');
-get_template_part('inc/blocks/menu-search-bar');
-get_template_part('inc/blocks/header');
-//get_template_part('inc/blocks/dirty-bird');
-get_template_part('inc/blocks/breaking-news');
-get_template_part('inc/blocks/feature-widget');
-get_template_part('inc/blocks/ad-and-two-dominant');
-get_template_part('inc/blocks/list-and-banter');
-get_template_part('inc/blocks/latest-videos');
-get_template_part('inc/blocks/old-homepage');
+//get_template_part('inc/blocks/leaderboard');
+//get_template_part('inc/blocks/menu-search-bar');
+//get_template_part('inc/blocks/header');
+get_template_part('inc/blocks/dirty-bird');
+//get_template_part('inc/blocks/breaking-news');
+//get_template_part('inc/blocks/feature-widget');
+//get_template_part('inc/blocks/ad-and-two-dominant');
+//get_template_part('inc/blocks/list-and-banter');
+//get_template_part('inc/blocks/latest-videos');
+//get_template_part('inc/blocks/old-homepage');
 
 
 
-get_template_part('inc/blocks/new-sports');
-get_footer();
+//get_template_part('inc/blocks/new-sports');
+//get_footer();
 
 ?>
 
