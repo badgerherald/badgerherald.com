5d6074101ec8844370166ce6f46b478741e82e3e
Aaron Wilson <awilson9@wisc.edu>
2016-03-31 22:26:22 -0500

    dirty bird updates

--- inc/blocks/dirty-bird.php
+++ inc/blocks/dirty-bird.php
@@ -1,7 +1,6 @@
 
 <div class="block dirty-bird-block" style='
-height:400px;
-position:absolute;'>
+'>
 <div class="wrapper"
 style='position:relative;'>
 
@@ -9,15 +8,16 @@ style='position:relative;'>
 
 <a href="dirtybird.badgerherald.com" >
 
-<img src = "" >
+<img src="<?php bloginfo('template_url') ?>/img/dirty-bird/xl.png" />
 <img src = "http://www.animatedimages.org/data/media/591/animated-parrot-image-0130.gif" 
 	style='
-
+	transform:scale(0.5, 0.5);
 	position:absolute;
-	bottom:-36px;
-	float:right;
+	bottom:0;
+	right:0;
+	align:right;
 '
->
+/>
 
 
 </a>
