97a93cf1effe577ba550243408fda964cef65eb1
Will Haynes <haynes24@gmail.com>
2018-06-09 13:55:59 -0500

    Moving app out of exa

--- functions/enqueue.php
+++ functions/enqueue.php
@@ -37,7 +37,7 @@ function _exa_enqueue_scripts_styles() {
 	//$mtime = filemtime(dirname(__FILE__) . '/../../js/components/exa.js');
 	wp_register_script('exa-wpapi', get_template_directory_uri() . '/js/vendor/wpapi.min.js','', $mtime, true );
 	wp_register_script('exa-moment', get_template_directory_uri() . '/js/vendor/moment.min.js','', $mtime, true );
-	wp_enqueue_script('exa-stencil', get_template_directory_uri() . '/js/components/exa.js',array('exa-wpapi','exa-moment'), $mtime, false );
+	wp_enqueue_script('exa-stencil', EXA_APP_URL . 'exa.js',array('exa-wpapi','exa-moment'), $mtime, false );
 
 	wp_localize_script('exa-wpapi', 'exa', array(
 		// define EXA_REST_URL in wp-config.php to debug against any server
--- index.php
+++ index.php
@@ -9,12 +9,12 @@ get_header();
 
 exa_container('leaderboard');
 exa_container('nameplate');
-//exa_container('breaking-news');
-//exa_container('feature-widget');
-//exa_container('ad-and-two-dominant');
-//exa_container('list-and-banter');
-//exa_container('latest-videos');
-//exa_container('old-homepage');
+exa_container('breaking-news');
+exa_container('feature-widget');
+exa_container('ad-and-two-dominant');
+exa_container('list-and-banter');
+exa_container('latest-videos');
+exa_container('old-homepage');
 
 get_footer();
 
