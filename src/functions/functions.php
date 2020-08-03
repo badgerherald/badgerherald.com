9207cc24b8e1dfc90710b3d1e5a006303c7a5801
Will Haynes <haynes24@gmail.com>
2020-08-03 08:00:39 -0500

    fixes

--- src/functions/functions.php
+++ src/functions/functions.php
@@ -613,13 +613,11 @@ add_filter('template_directory_uri','_hexa_enforce_https_in_template_urls');
  * Enqueue hexa scripts and styles.
  */
 function hexa_scripts() {
-    $mtime = filemtime(dirname(__FILE__) . '/js/hexa.js') ?: "";
-    wp_enqueue_script('hexa-script', get_stylesheet_directory_uri() . '/js/hexa.js',array('jquery'),$mtime,true);
+    wp_enqueue_script('hexa-script', get_stylesheet_directory_uri() . '/js/hexa.js',array('jquery'),"",true);
     
     wp_enqueue_script('', 'https://js.stripe.com/v3/',null,null,false);
     
-    $mtime = filemtime(dirname(__FILE__) . '/style.css') ?: "";
-    wp_enqueue_style('hexa-style', get_stylesheet_directory_uri().'/style.css', array('exa-style'),$mtime);
+    wp_enqueue_style('hexa-style', get_stylesheet_directory_uri().'/style.css', array('exa-style'),"");
 }
 add_action('wp_enqueue_scripts', 'hexa_scripts');
 
