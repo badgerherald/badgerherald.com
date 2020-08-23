3a3eb07471b712bf65bbd12d66ad4db593d2cd68
Will Haynes <haynes24@gmail.com>
2020-08-23 12:24:04 -0500

    fixing ad loads

--- src/js/exa.js
+++ src/js/exa.js
@@ -193,22 +193,6 @@ jQuery(document).ready(function($) {
 		return vars;
 	}
 
-	//Smooth scrolling to anchors from anchor links on same page.
-	$(function() {
-	  $('a[href*=#]:not([href=#])').click(function() {
-		if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-		  var target = $(this.hash);
-		  target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-		  if (target.length) {
-			$('html,body').animate({
-			  scrollTop: target.offset().top - 72
-			}, 1000);
-			return false;
-		  }
-		}
-	  });
-	});
-
 	$(window).scroll(function() {
 		$(window).resize();
 	});
