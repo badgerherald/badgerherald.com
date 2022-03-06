7e532021b9e438e14eed4f93074487fd3e2a70b8
Will Haynes <will@wjh.dev>
2022-03-06 14:25:46 -0600

    whoops

--- src/theme/functions/inc/enqueue.php
+++ src/theme/functions/inc/enqueue.php
@@ -55,7 +55,7 @@ function wpse33318_tiny_mce_before_init( $mce_init ) {
         $version = "";
     }
 	
-    $mce_init['cache_suffix'] = 'v=123';
+    $mce_init['cache_suffix'] = 'v=' . $version;
     return $mce_init;    
 }
 add_filter( 'tiny_mce_before_init', 'wpse33318_tiny_mce_before_init' );
