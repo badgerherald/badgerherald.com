e3259d658315a369f26b45e99f994eaace8b161b
Will Haynes <will@wjh.dev>
2022-08-27 12:22:51 -0500

    adding nr-agent (#255)

--- config/wordpress/docker-entrypoint.sh
+++ config/wordpress/docker-entrypoint.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
-# 0/ re-permission media-kit
-chown -R www-data:www-data /var/www/html/media-kit
+# 1/ configure new-relic php agent
+sed -i -e "s/REPLACE_WITH_REAL_KEY/$NRIA_LICENSE_KEY/" \
+    -e "s/newrelic.appname.*/newrelic.appname=\"$NRIA_APP_NAME\"/" \
+    $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini
 
-# 1/ dump ENV variables into cron environment
+# 2/ dump ENV variables into cron environment
 printenv > /etc/environment
 
-# 2/ start cron service
+# 3/ start cron service
 service cron start
