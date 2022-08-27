6c4d31a161378d9035389e3ff7402570efb5b1ba
Will Haynes <will@wjh.dev>
2022-08-27 12:04:26 -0500

    newrelic env vars, not php defines

--- config/wordpress/docker-entrypoint.sh
+++ config/wordpress/docker-entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # 1/ configure new-relic php agent
-sed -i -e "s/REPLACE_WITH_REAL_KEY/${NRIA_LICENSE_KEY}/" \
-    -e "s/newrelic.appname.*/newrelic.appname=\"${COMPOSE-PROJECT-NAME}-wordpress\/app\"/" \
+sed -i -e "s/REPLACE_WITH_REAL_KEY/$NRIA_LICENSE_KEY/" \
+    -e "s/newrelic.appname.*/newrelic.appname=\"$NRIA_APP_NAME\"/" \
     $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini
 
 # 2/ dump ENV variables into cron environment
