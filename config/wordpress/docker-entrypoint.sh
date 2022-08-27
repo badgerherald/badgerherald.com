3330818bd84ae5aefb4f98b9d85dace332677004
Will Haynes <will@wjh.dev>
2022-08-27 11:45:15 -0500

    escape slash

--- config/wordpress/docker-entrypoint.sh
+++ config/wordpress/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 
 # 1/ configure new-relic php agent
 sed -i -e "s/REPLACE_WITH_REAL_KEY/${NRIA_LICENSE_KEY}/" \
-    -e "s/newrelic.appname.*/newrelic.appname=\"${COMPOSE-PROJECT-NAME}-wordpress/app\"/" \
+    -e "s/newrelic.appname.*/newrelic.appname=\"${COMPOSE-PROJECT-NAME}-wordpress\/app\"/" \
     $(php -r "echo(PHP_CONFIG_FILE_SCAN_DIR);")/newrelic.ini
 
 # 2/ dump ENV variables into cron environment
