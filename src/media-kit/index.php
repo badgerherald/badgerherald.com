bfa7c7615504ea19609cd107ab26ad5a5f33dc88
Will Haynes <will@wjh.dev>
2022-09-03 21:44:30 -0500

    more warnings...

--- src/media-kit/index.php
+++ src/media-kit/index.php
@@ -1,59 +1,71 @@
 <!DOCTYPE html>
-<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
-	<head>
+<html>
 
-		<meta charset="utf-8">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+<head>
 
-		<title>Advertise with The Badger Herald</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 
-		<meta name="description" content="">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Advertise with The Badger Herald</title>
 
-		<!-- Google Fonts (PT SANS NARROW BOLD/PT SERIF BOLD+BOLD-ITALIC) -->
-		<link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700|PT+Serif:700,700italic' rel='stylesheet' type='text/css'>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-		<link rel="stylesheet" href="css/main.css">
-		
-		<script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
-		<!-- Open Graph Tags: http://ogp.me -->
-		<meta property='og:title' content='Advertise with The Badger Herald' />
-		<meta property="og:description" content="We can connect your brand to the campus market." />
-		<meta property='og:site_name' content='The Badger Herald' />
-		<meta property='og:type' content='website' />
-		<meta property='og:url' content='http://advertise.badgerherald.com/' />
-		<meta property='og:image' content='http://advertise.badgerherald.com/img/og.png' />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href='http://fonts.googleapis.com/css?family=PT+Sans+Narrow:700|PT+Serif:700,700italic' rel='stylesheet'
+        type='text/css'>
 
-	</head>
+    <link rel="stylesheet" href="css/main.css">
 
-	<body>
-	<!-- Main jumbotron for a primary marketing message or call to action -->
+    <script src="js/vendor/modernizr-2.6.2-respond-1.1.0.min.js"></script>
 
-	<?php include('slides/hero.php'); ?>
-	<?php include('slides/nav.php'); ?>
-	<?php include('slides/about.php'); ?>
-	<?php include('slides/digital.php'); ?>
-	<?php include('slides/email.php'); ?>
-	<?php include('slides/contact.php'); ?>
+    <!-- Open Graph Tags: http://ogp.me -->
+    <meta property='og:title' content='Advertise with The Badger Herald' />
+    <meta property="og:description" content="We can connect your brand to the campus market." />
+    <meta property='og:site_name' content='The Badger Herald' />
+    <meta property='og:type' content='website' />
+    <meta property='og:url' content='http://advertise.badgerherald.com/' />
+    <meta property='og:image' content='http://advertise.badgerherald.com/img/og.png' />
 
-	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-		<script>window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')</script>
+</head>
 
-		<script src="js/vendor/jquery-ui/jquery-ui.min.js"></script>
-		<script src="js/main.js"></script>
+<body>
+    <!-- Main jumbotron for a primary marketing message or call to action -->
 
-		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
-		<script>
-				(function(b,o,i,l,e,r){b.GoogleAnalyticsObject=l;b[l]||(b[l]=
-				function(){(b[l].q=b[l].q||[]).push(arguments)});b[l].l=+new Date;
-				e=o.createElement(i);r=o.getElementsByTagName(i)[0];
-				e.src='//www.google-analytics.com/analytics.js';
-				r.parentNode.insertBefore(e,r)}(window,document,'script','ga'));
-				ga('create','UA-XXXXX-X');ga('send','pageview');
-		</script>
-	</body>
+    <?php include('slides/hero.php'); ?>
+    <?php include('slides/nav.php'); ?>
+    <?php include('slides/about.php'); ?>
+    <?php include('slides/digital.php'); ?>
+    <?php include('slides/email.php'); ?>
+    <?php include('slides/contact.php'); ?>
+
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+    <script>
+    window.jQuery || document.write('<script src="js/vendor/jquery-1.11.1.min.js"><\/script>')
+    </script>
+
+    <script src="js/vendor/jquery-ui/jquery-ui.min.js"></script>
+    <script src="js/main.js"></script>
+
+    <!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+    <script>
+    (function(b, o, i, l, e, r) {
+        b.GoogleAnalyticsObject = l;
+        b[l] || (b[l] =
+            function() {
+                (b[l].q = b[l].q || []).push(arguments)
+            });
+        b[l].l = +new Date;
+        e = o.createElement(i);
+        r = o.getElementsByTagName(i)[0];
+        e.src = '//www.google-analytics.com/analytics.js';
+        r.parentNode.insertBefore(e, r)
+    }(window, document, 'script', 'ga'));
+    ga('create', 'UA-XXXXX-X');
+    ga('send', 'pageview');
+    </script>
+</body>
+
 </html>
--- src/theme/functions/functions.php
+++ src/theme/functions/functions.php
@@ -25,7 +25,6 @@ include( __DIR__ . '/inc/embeds.php');
 include( __DIR__ . '/inc/inline-links.php');
 include( __DIR__ . '/inc/mastheads.php');
 include( __DIR__ . '/inc/galleries.php');
-include( __DIR__ . '/inc/analytic-dashboard.php');
 include( __DIR__ . '/inc/headlines.php');
 include( __DIR__ . '/inc/pullquotes.php');
 include( __DIR__ . '/inc/social.php');
