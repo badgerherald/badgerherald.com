a74c6e6ea89e261ef5f5dea7dc1615583f993089
Will Haynes <will@wjh.dev>
2022-09-19 17:19:32 -0500

    bump memory limit?

--- src/components.d.ts
+++ src/components.d.ts
@@ -5,35 +5,10 @@
  * It contains typing information for all components that exist in this project.
  */
 import { HTMLStencilElement, JSXBase } from "@stencil/core/internal";
-import { Connection, Query, SearchResult, Template, Theme } from "@webpress/core";
 import { AdMapping } from "./components/model/AdMapping";
+import { Connection, Template, Theme } from "@webpress/core";
 export namespace Components {
     interface BhImports {
-    }
-    interface BhPopularPosts {
-        "global": Connection.Context;
-        "size": number;
-    }
-    interface BhSearchForm {
-        "focused": boolean;
-        "term": string;
-    }
-    interface BhSearchPagination {
-        "pagination": Query.Pagination<SearchResult>;
-    }
-    interface BhSearchResultOccurances {
-        "result": SearchResult;
-        "searchQuery": String;
-    }
-    interface BhSearchResultTitle {
-        "result": SearchResult;
-        "searchQuery": String;
-    }
-    interface BhSearchResults {
-        "connection": Connection;
-    }
-    interface BhSearchResultsPage {
-        "global": Connection.Context;
     }
     interface BstAdSlot {
         "adUnitPath": string;
@@ -50,6 +25,9 @@ export namespace Components {
         "query": Template.Query;
         "searchQuery": string;
         "theme": Theme;
+    }
+    interface ExaSearchForm {
+        "focused": boolean;
     }
     interface HrldArticleSidebar {
     }
@@ -69,48 +47,6 @@ declare global {
         prototype: HTMLBhImportsElement;
         new (): HTMLBhImportsElement;
     };
-    interface HTMLBhPopularPostsElement extends Components.BhPopularPosts, HTMLStencilElement {
-    }
-    var HTMLBhPopularPostsElement: {
-        prototype: HTMLBhPopularPostsElement;
-        new (): HTMLBhPopularPostsElement;
-    };
-    interface HTMLBhSearchFormElement extends Components.BhSearchForm, HTMLStencilElement {
-    }
-    var HTMLBhSearchFormElement: {
-        prototype: HTMLBhSearchFormElement;
-        new (): HTMLBhSearchFormElement;
-    };
-    interface HTMLBhSearchPaginationElement extends Components.BhSearchPagination, HTMLStencilElement {
-    }
-    var HTMLBhSearchPaginationElement: {
-        prototype: HTMLBhSearchPaginationElement;
-        new (): HTMLBhSearchPaginationElement;
-    };
-    interface HTMLBhSearchResultOccurancesElement extends Components.BhSearchResultOccurances, HTMLStencilElement {
-    }
-    var HTMLBhSearchResultOccurancesElement: {
-        prototype: HTMLBhSearchResultOccurancesElement;
-        new (): HTMLBhSearchResultOccurancesElement;
-    };
-    interface HTMLBhSearchResultTitleElement extends Components.BhSearchResultTitle, HTMLStencilElement {
-    }
-    var HTMLBhSearchResultTitleElement: {
-        prototype: HTMLBhSearchResultTitleElement;
-        new (): HTMLBhSearchResultTitleElement;
-    };
-    interface HTMLBhSearchResultsElement extends Components.BhSearchResults, HTMLStencilElement {
-    }
-    var HTMLBhSearchResultsElement: {
-        prototype: HTMLBhSearchResultsElement;
-        new (): HTMLBhSearchResultsElement;
-    };
-    interface HTMLBhSearchResultsPageElement extends Components.BhSearchResultsPage, HTMLStencilElement {
-    }
-    var HTMLBhSearchResultsPageElement: {
-        prototype: HTMLBhSearchResultsPageElement;
-        new (): HTMLBhSearchResultsPageElement;
-    };
     interface HTMLBstAdSlotElement extends Components.BstAdSlot, HTMLStencilElement {
     }
     var HTMLBstAdSlotElement: {
@@ -128,6 +64,12 @@ declare global {
     var HTMLExaNameplateMenusElement: {
         prototype: HTMLExaNameplateMenusElement;
         new (): HTMLExaNameplateMenusElement;
+    };
+    interface HTMLExaSearchFormElement extends Components.ExaSearchForm, HTMLStencilElement {
+    }
+    var HTMLExaSearchFormElement: {
+        prototype: HTMLExaSearchFormElement;
+        new (): HTMLExaSearchFormElement;
     };
     interface HTMLHrldArticleSidebarElement extends Components.HrldArticleSidebar, HTMLStencilElement {
     }
@@ -161,16 +103,10 @@ declare global {
     };
     interface HTMLElementTagNameMap {
         "bh-imports": HTMLBhImportsElement;
-        "bh-popular-posts": HTMLBhPopularPostsElement;
-        "bh-search-form": HTMLBhSearchFormElement;
-        "bh-search-pagination": HTMLBhSearchPaginationElement;
-        "bh-search-result-occurances": HTMLBhSearchResultOccurancesElement;
-        "bh-search-result-title": HTMLBhSearchResultTitleElement;
-        "bh-search-results": HTMLBhSearchResultsElement;
-        "bh-search-results-page": HTMLBhSearchResultsPageElement;
         "bst-ad-slot": HTMLBstAdSlotElement;
         "exa-menu-button": HTMLExaMenuButtonElement;
         "exa-nameplate-menus": HTMLExaNameplateMenusElement;
+        "exa-search-form": HTMLExaSearchFormElement;
         "hrld-article-sidebar": HTMLHrldArticleSidebarElement;
         "hrld-homepage-leaderboard": HTMLHrldHomepageLeaderboardElement;
         "hrld-homepage-sidekick": HTMLHrldHomepageSidekickElement;
@@ -180,31 +116,6 @@ declare global {
 }
 declare namespace LocalJSX {
     interface BhImports {
-    }
-    interface BhPopularPosts {
-        "global"?: Connection.Context;
-        "size"?: number;
-    }
-    interface BhSearchForm {
-        "focused"?: boolean;
-        "term"?: string;
-    }
-    interface BhSearchPagination {
-        "pagination"?: Query.Pagination<SearchResult>;
-    }
-    interface BhSearchResultOccurances {
-        "result"?: SearchResult;
-        "searchQuery"?: String;
-    }
-    interface BhSearchResultTitle {
-        "result"?: SearchResult;
-        "searchQuery"?: String;
-    }
-    interface BhSearchResults {
-        "connection"?: Connection;
-    }
-    interface BhSearchResultsPage {
-        "global"?: Connection.Context;
     }
     interface BstAdSlot {
         "adUnitPath": string;
@@ -222,6 +133,9 @@ declare namespace LocalJSX {
         "searchQuery"?: string;
         "theme"?: Theme;
     }
+    interface ExaSearchForm {
+        "focused"?: boolean;
+    }
     interface HrldArticleSidebar {
     }
     interface HrldHomepageLeaderboard {
@@ -234,16 +148,10 @@ declare namespace LocalJSX {
     }
     interface IntrinsicElements {
         "bh-imports": BhImports;
-        "bh-popular-posts": BhPopularPosts;
-        "bh-search-form": BhSearchForm;
-        "bh-search-pagination": BhSearchPagination;
-        "bh-search-result-occurances": BhSearchResultOccurances;
-        "bh-search-result-title": BhSearchResultTitle;
-        "bh-search-results": BhSearchResults;
-        "bh-search-results-page": BhSearchResultsPage;
         "bst-ad-slot": BstAdSlot;
         "exa-menu-button": ExaMenuButton;
         "exa-nameplate-menus": ExaNameplateMenus;
+        "exa-search-form": ExaSearchForm;
         "hrld-article-sidebar": HrldArticleSidebar;
         "hrld-homepage-leaderboard": HrldHomepageLeaderboard;
         "hrld-homepage-sidekick": HrldHomepageSidekick;
@@ -256,16 +164,10 @@ declare module "@stencil/core" {
     export namespace JSX {
         interface IntrinsicElements {
             "bh-imports": LocalJSX.BhImports & JSXBase.HTMLAttributes<HTMLBhImportsElement>;
-            "bh-popular-posts": LocalJSX.BhPopularPosts & JSXBase.HTMLAttributes<HTMLBhPopularPostsElement>;
-            "bh-search-form": LocalJSX.BhSearchForm & JSXBase.HTMLAttributes<HTMLBhSearchFormElement>;
-            "bh-search-pagination": LocalJSX.BhSearchPagination & JSXBase.HTMLAttributes<HTMLBhSearchPaginationElement>;
-            "bh-search-result-occurances": LocalJSX.BhSearchResultOccurances & JSXBase.HTMLAttributes<HTMLBhSearchResultOccurancesElement>;
-            "bh-search-result-title": LocalJSX.BhSearchResultTitle & JSXBase.HTMLAttributes<HTMLBhSearchResultTitleElement>;
-            "bh-search-results": LocalJSX.BhSearchResults & JSXBase.HTMLAttributes<HTMLBhSearchResultsElement>;
-            "bh-search-results-page": LocalJSX.BhSearchResultsPage & JSXBase.HTMLAttributes<HTMLBhSearchResultsPageElement>;
             "bst-ad-slot": LocalJSX.BstAdSlot & JSXBase.HTMLAttributes<HTMLBstAdSlotElement>;
             "exa-menu-button": LocalJSX.ExaMenuButton & JSXBase.HTMLAttributes<HTMLExaMenuButtonElement>;
             "exa-nameplate-menus": LocalJSX.ExaNameplateMenus & JSXBase.HTMLAttributes<HTMLExaNameplateMenusElement>;
+            "exa-search-form": LocalJSX.ExaSearchForm & JSXBase.HTMLAttributes<HTMLExaSearchFormElement>;
             "hrld-article-sidebar": LocalJSX.HrldArticleSidebar & JSXBase.HTMLAttributes<HTMLHrldArticleSidebarElement>;
             "hrld-homepage-leaderboard": LocalJSX.HrldHomepageLeaderboard & JSXBase.HTMLAttributes<HTMLHrldHomepageLeaderboardElement>;
             "hrld-homepage-sidekick": LocalJSX.HrldHomepageSidekick & JSXBase.HTMLAttributes<HTMLHrldHomepageSidekickElement>;
