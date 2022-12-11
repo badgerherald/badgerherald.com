6404c2c37bdc8d326fdff9eaa487ef6f649e2fad
Will Haynes <will@wjh.dev>
2022-12-11 12:25:35 -0600

    configurable memory usage

--- config/wordpress/docker-entrypoint.sh
+++ config/wordpress/docker-entrypoint.sh
@@ -8,5 +8,25 @@ sed -i -e "s/REPLACE_WITH_REAL_KEY/$NRIA_LICENSE_KEY/" \
 # 2/ dump ENV variables into cron environment
 printenv > /etc/environment
 
-# 3/ start cron service
+# 3/ create php-fpm.conf
+cat > /usr/local/etc/php-fpm.d/zzz-my-extra.conf <<ENDPHPFPMCONF
+[www]
+pm = static
+pm.max_children = $PHP_FPM_MAX_CHILDREN
+pm.max_requests = 500
+ENDPHPFPMCONF
+
+# 4/ create php.ini
+cat > /usr/local/etc/php/conf.d/extra.ini <<ENDPHPINI
+file_uploads = On
+memory_limit = $PHP_MEMORY_LIMIT
+upload_max_filesize = 6M
+post_max_size = 64M
+max_execution_time = 600
+
+opcache.max_accelerated_files=20000
+opcache.memory_consumption=$PHP_OPCACHE_MEMORY_LIMIT
+ENDPHPINI
+
+# 5/ start cron service
 service cron start
