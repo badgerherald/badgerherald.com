32fa699e91d42ce6e3a3253d808f2edec0f77a85
Will Haynes <will@wjh.dev>
2022-12-11 10:53:02 -0600

    bump

--- src/theme/scripts/index.asset.php
+++ src/theme/scripts/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-element', 'wp-hooks', 'wp-polyfill'), 'version' => 'b2df15bb4cfe3ffb508f6ce259eaa0eb');
+<?php return array('dependencies' => array('react', 'wp-components', 'wp-compose', 'wp-core-data', 'wp-hooks', 'wp-polyfill'), 'version' => 'd014be6e0e9f86abbf5c99d2c583f87a');
